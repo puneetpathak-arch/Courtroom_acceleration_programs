7f84978e7c51693eda70595184ad15a2f4326ca7
Puneet Pathak <pp4july2006@gmail.com>
2025-11-08 19:22:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "gen-lang-client-0538874991",
+  "appId": "1:296181927103:web:d8feb1b6dfa6b590b1ca0a",
+  "apiKey": "AIzaSyCPnOAeIQ6KLqYXOdOghoDht4F1rt46v1I",
+  "authDomain": "gen-lang-client-0538874991.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "296181927103"
+};
